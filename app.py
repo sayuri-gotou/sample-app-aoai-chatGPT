74eb0b53bcedf1cc5d807634de2627477165742a
sayuri-gotou <s-gotou@core.co.jp>
2024-01-30 14:11:24 +0900

    AOAI接続の際のログ確認

--- app.py
+++ app.py
@@ -45,24 +45,6 @@ DEBUG = os.environ.get("DEBUG", "false")
 DEBUG_LOGGING = DEBUG.lower() == "true"
 if DEBUG_LOGGING:
     logging.basicConfig(level=logging.DEBUG)
-    
-config_integration.trace_integrations(['logging'])
-
-logger = logging.getLogger(__name__)
-
-logger.setLevel(logging.DEBUG)
-APPLICATIONINSIGHTS_CONNECTION_STRING = os.environ.get("APPLICATIONINSIGHTS_CONNECTION_STRING")
-handler = AzureLogHandler(connection_string=APPLICATIONINSIGHTS_CONNECTION_STRING)
-handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
-logger.addHandler(handler)
-
-tracer = Tracer(
-    exporter=AzureExporter(connection_string=APPLICATIONINSIGHTS_CONNECTION_STRING),
-    sampler=ProbabilitySampler(1.0),
-)
-
-with tracer.span(name='test'):
-	print('Hello, World!')
 
 
 # On Your Data Settings
@@ -531,6 +513,7 @@ def conversation_with_data(request_body):
     history_metadata = request_body.get("history_metadata", {})
 
     if not SHOULD_STREAM:
+    	logging.debug("if not Should_stream")
         r = requests.post(endpoint, headers=headers, json=body)
         status_code = r.status_code
         r = r.json()
@@ -543,6 +526,7 @@ def conversation_with_data(request_body):
             return Response(format_as_ndjson(result), status=status_code)
 
     else:
+    	logging.debug("else")
         return Response(stream_with_data(body, headers, endpoint, history_metadata), mimetype='text/event-stream')
 
 def stream_without_data(response, history_metadata={}):
